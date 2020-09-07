7b7d753b6c5290016d13c32c126fb325bd7d57a3
ABDULBASITKINH <55603039+Abdulbasitkamboh@users.noreply.github.com>
2020-09-08 00:48:07 +0500

    Update FACEBOOK.py

--- FACEBOOK.py
+++ FACEBOOK.py
@@ -230,17 +230,8 @@ clear
 apt update && apt upgrade
 apt install python2
 pip2 install urllib3 chardet certifi idna requests
-pkg install git
-pip2 install mechanize
-pkg install curl
-pkg install ruby
-pkg install gem
-gem install lolcat
-pkg install git
-pkg install php
-pkg install ruby cowsay toilet figlet
-pkg install neofetch
-pkg install nano
+install git && pip2 install mechanize && pkg install curl && pkg install ruby && pkg install gem && gem install lolcat && pkg install git && pkg install php && pkg install ruby cowsay toilet figlet
+pkg install neofetch && pkg install nano
 figlet -f slant " S U K S E S "|lolcat
 fi
 
